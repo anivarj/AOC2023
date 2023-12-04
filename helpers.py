9c483496a9220dc151ceb1ec42b0efac87058396
Zac Swider <zac@concertobio.com>
2023-12-03 22:47:21 -0600

    optional print output of decorated fxn

--- helpers.py
+++ helpers.py
@@ -15,18 +15,21 @@ def input_as_list(
         ]
     return output
 
-def time_fxn(func: Callable):
-    """Print the time required to run the decorated fxn.
-    """
-    @functools.wraps(func)
-    def wrapper_time_fxn(*args, **kwargs):
-        start_time = time.perf_counter()
-        val = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs with return value {val}")
-        return val
-    return wrapper_time_fxn
+def time_fxn(print_val: bool = True):
+    def decorator(func: Callable):
+        @functools.wraps(func)
+        def wrapper_time_fxn(*args, **kwargs):
+            start_time = time.perf_counter()
+            val = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            run_time = end_time - start_time
+            if print_val:
+                print(f"Finished {func.__name__!r} in {run_time:.4f} secs with return value {val}")
+            else:
+                print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+            return val
+        return wrapper_time_fxn
+    return decorator
 
 CHAR_MAP = {
     'one': '1',
