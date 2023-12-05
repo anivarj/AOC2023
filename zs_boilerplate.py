1ab0c5c883c5886a8edc4db246e24014eb601cb1
Zac Swider <zac@concertobio.com>
2023-12-04 22:02:33 -0600

    boilerplate template for all days

---
+++ zs_boilerplate.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from helpers import (
+    input_as_list,
+    time_fxn,
+)
+
+@time_fxn()
+def main(
+    input_file_path: Path
+) -> str:
+    res = shared_computation(
+        input_file_path,
+    )
+    part_1()
+    part_2()
+    return 'yay, finished!'
+
+@time_fxn(print_val=False)
+def shared_computation(
+) -> None:
+    ...
+
+@time_fxn()
+def part_1(
+) -> None:
+    ...
+
+@time_fxn()
+def part_2(
+) -> None:
+    ...
+
+if __name__ == "__main__":
+    main(
+        Path('')
+    )
